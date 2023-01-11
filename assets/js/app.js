6add1cba4ac5a03541557c3db9c6845ca8571aff
Gabriel Rocha <gabriel.rocha20@live.com>
2023-01-11 14:03:37 -0300

    Header Responsivo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,20 @@
-function acionarMenuHamburguer(){
-    let menu = document.getElementById('menu_hamburguer')
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-      } else {
-        menu.style.display = "block";
-      }
+function acionarMenuHamburguer() {
+  let menu = document.getElementById('menu_hamburguer')
+  let box = document.getElementById('box');
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+    box.style.borderRadius = "0px 0px 25px 25px";
+  } else {
+    menu.style.display = "block";
+    box.style.borderRadius = "0px 0px 0px 0px";
+  }
+}
+
+function mudarBordaMenu(){
+  let menu = document.getElementById('menu_hamburguer')
+  let box = document.getElementById('box');
+  if(window.innerWidth > 768){
+    box.style.borderRadius = "0px 0px 25px 25px";
+    menu.style.display = "none";
+  }
 }
