44fb97d4659a2eb071cb764ff3f23c94c7471940
Gabriel Rocha <gabriel.rocha20@live.com>
2023-02-27 16:39:51 -0300

    Novo header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,6 @@
-function acionarMenuHamburguer() {
-  let menu = document.getElementById('menu_hamburguer')
-  let box = document.getElementById('box');
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-    box.style.borderRadius = "0px 0px 25px 25px";
-  } else {
-    menu.style.display = "block";
-    box.style.borderRadius = "0px 0px 0px 0px";
-  }
-}
+function mudaEstadoMenu() {
+    let nav = document.getElementsByClassName('header__nav');
+    console.log(nav[0])
 
-function mudarBordaMenu(){
-  let menu = document.getElementById('menu_hamburguer')
-  let box = document.getElementById('box');
-  if(window.innerWidth > 768){
-    box.style.borderRadius = "0px 0px 25px 25px";
-    menu.style.display = "none";
-  }
+    nav[0].classList.toggle("header__nav--desativado");
 }
